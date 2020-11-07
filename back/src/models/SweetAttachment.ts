6171b5dcfcd35e53292b92c7299e3905d98d89b7
zahmis <meda829ratteru86@gmail.com>
2020-11-07 08:54:51 +0000

    fix model

--- back/src/models/SweetAttachment.ts
+++ back/src/models/SweetAttachment.ts
@@ -1,16 +1,20 @@
 import {Integer} from "@tsed/common"
-import {Model,ObjectID} from "@tsed/mongoose";
+import {Model,ObjectID, Ref} from "@tsed/mongoose";
+import { Sweet } from './Sweet';
 
 @Model()
 export class SweetAttachment {
 
+    @ObjectID("id")
+    _id: string
+      
     filePath: string;
     fileName: string;
     fileFormat: string;
 
-    @ObjectID("id")
-    _id: string;
-
+    @Ref(Sweet)
+    sweetId: Ref<Sweet>
+  
     @Integer()
     filesize: number
 }
